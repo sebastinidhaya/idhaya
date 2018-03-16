4e647bd336377de86c9f47fcbdfcc301682e0af6
sebastin <sebastinidhayaraja@gmail.com>
2018-03-16 16:57:01 +0530

    2nd level bug
    this code is removed the dublicate date of union and get each data our equalent but it get also dublicate value on final result in same day credit and debit

--- mysqQuery/credit_debit_combinaion.sql
+++ mysqQuery/credit_debit_combinaion.sql
@@ -1,35 +1,52 @@
-SELECT
-          tabkey.PAYMENT_DATE
-        , tabkey.DRIVER_ID
-        , IFNULL(ADVANCE_PAYMENT.AMOUNT, 0) AS DEBIT
-        , IFNULL(ADVANCE_CREDIT.AMOUNT, 0)  AS CREDIT
-FROM
-          (
-                 SELECT
-                        ADVANCE_PAYMENT.PAYMENT_DATE
-                      , ADVANCE_PAYMENT.CREATED_DATE
-                      , ADVANCE_PAYMENT.DRIVER_ID
-                 FROM
-                        ADVANCE_PAYMENT
-                 WHERE
-                        ADVANCE_PAYMENT.DRIVER_ID = 6
-                 UNION
-                 SELECT
-                        ADVANCE_CREDIT.CREDIT_DATE
-                      , ADVANCE_CREDIT.CREATED_DATE
-                      , ADVANCE_CREDIT.DRIVER_ID
-                 FROM
-                        ADVANCE_CREDIT
-                 WHERE
-                        ADVANCE_CREDIT.DRIVER_ID = 6
-          )
-          AS tabkey
-          LEFT JOIN
-                    ADVANCE_PAYMENT
-                    ON
-                              tabkey.CREATED_DATE = ADVANCE_PAYMENT.CREATED_DATE
-          LEFT JOIN
-                    ADVANCE_CREDIT
-                    ON
-                              tabkey.CREATED_DATE = ADVANCE_CREDIT.CREATED_DATE
-;
+DELIMITER $$ USE `transport`$$ DROP PROCEDURE
+IF EXISTS `ADVANCE_DRIVER_LEDGER`$$ CREATE DEFINER=`root`@`localhost`
+PROCEDURE `ADVANCE_DRIVER_LEDGER`
+                                 (
+                                     IN DRIV_ID INT
+                                 )
+    BEGIN
+        /*SELECT DATE_FORMAT(PAYMENT_DATE, '%d-%m-%Y') AS PAYMENT_DATE, AMOUNT, REMARKS FROM ADVANCE_PAYMENT WHERE DRIVER_ID = ID and
+        MONTH(PAYMENT_DATE) = MONTH(CURRENT_DATE()) AND YEAR(PAYMENT_DATE) = YEAR(CURRENT_DATE()) ORDER BY PAYMENT_DATE;*/
+        SELECT
+                  tabkey.PAYMENT_DATE
+                , tabkey.REMARKS
+                , IFNULL(ADVANCE_PAYMENT.AMOUNT, 0) AS DEBIT
+                , IFNULL(ADVANCE_CREDIT.AMOUNT, 0)  AS CREDIT
+        FROM
+                  (
+                           SELECT
+                                    ADVANCE_PAYMENT.PAYMENT_DATE
+                                  , ADVANCE_PAYMENT.REMARKS
+                                  , ADVANCE_PAYMENT.DRIVER_ID
+                           FROM
+                                    ADVANCE_PAYMENT
+                           WHERE
+                                    ADVANCE_PAYMENT.DRIVER_ID = DRIV_ID
+                           GROUP BY
+                                    ADVANCE_PAYMENT.PAYMENT_DATE
+                           UNION ALL
+                           SELECT
+                                    ADVANCE_CREDIT.CREDIT_DATE
+                                  , ADVANCE_CREDIT.REMARKS
+                                  , ADVANCE_CREDIT.DRIVER_ID
+                           FROM
+                                    ADVANCE_CREDIT
+                           WHERE
+                                    ADVANCE_CREDIT.DRIVER_ID = DRIV_ID
+                           GROUP BY
+                                    ADVANCE_CREDIT.CREDIT_DATE
+                  )
+                  AS tabkey
+                  LEFT JOIN
+                            ADVANCE_PAYMENT
+                            ON
+                                      tabkey.PAYMENT_DATE           = ADVANCE_PAYMENT.PAYMENT_DATE
+                                      AND ADVANCE_PAYMENT.DRIVER_ID = DRIV_ID
+                  LEFT JOIN
+                            ADVANCE_CREDIT
+                            ON
+                                      tabkey.PAYMENT_DATE          = ADVANCE_CREDIT.CREDIT_DATE
+                                      AND ADVANCE_CREDIT.DRIVER_ID = DRIV_ID
+        ;
+        
+        END$$ DELIMITER;
